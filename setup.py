fece2a6667dd368133d9f9871fea1308132def94
Rachit Singh <rachitsingh150@gmail.com>
2025-03-20 21:30:59 +0530

    requirements , setup files added

---
+++ setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+
+__version__ = "0.0.0"
+
+REPO_NAME = "Brain-Tumor-Classification"
+AUTHOR_USER_NAME = "rachit3508"
+SRC_REPO = "Brain-Tumor-Classification"
+AUTHOR_EMAIL = "rachitsingh150@gmail.com"
+
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A package for doing the Brain Tumor Classification",
+    long_description=long_description,
+    long_description_content="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src")
+)
